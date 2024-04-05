6499a5e0b6be904294dfdc6a98e62bc02dccd18d
Preeti <preetiyadav5443@gmail.com>
2024-04-05 12:19:33 +0530

    changes in column chart

---
+++ pages/findings.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="lg:pl-[54px] lg:pr-[63px] md:px-10 px-5 pt-[36px] w-full">
+        <FindingsColumnChart />
+    
+    </div>
+
+</template>
+<script setup lang="ts">
+// Page Meta
+definePageMeta({
+    layout: "home",
+});
+</script>
