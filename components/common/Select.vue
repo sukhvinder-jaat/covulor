ea69e7686716b8d7dcb63aecfb0d1f352aded627
kapil verma <vermakp39@gmail.com>
2024-04-05 15:25:03 +0530

    select workink

---
+++ components/common/Select.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { defineProps, ref } from 'vue';
+
+const countries = ['United States', 'Canada', 'Mexico'];
+
+const props = defineProps({
+    options: Array,
+});
+const country = ref(props.options ? props.options[0] : null);
+</script>
+
+<template>
+    <div class=" w-full">
+        <USelect :options="options" />
+    </div>
+</template>
