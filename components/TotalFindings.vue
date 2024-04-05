9fe6bd2c80fdf95b2eb8e7bec7050a3bc2482d80
sukhvinder_jaat <122784772+sukhvinder-jaat@users.noreply.github.com>
2024-04-05 15:26:14 +0530

    no change

--- components/TotalFindings.vue
+++ components/TotalFindings.vue
@@ -34,13 +34,80 @@
       >
         <DashboardCircularChart />
       </div>
+      <CommonTable />
     </div>
   </div>
-  <CommonTable />
+  <CommonTable :rows="heading" :columns="data" />
 </template>
 
 <script setup lang="ts">
 const year = ["2023", "2022", "2021", "2020"];
 
 const selected = ref(year[0]);
+const heading = [
+  {
+    key: "id",
+    label: "ID",
+  },
+  {
+    key: "name",
+    label: "User name",
+  },
+  {
+    key: "title",
+    label: "Job position",
+  },
+  {
+    key: "email",
+    label: "Email",
+  },
+  {
+    key: "role",
+  },
+];
+
+const data = [
+  {
+    id: 1,
+    name: "Lindsay Walton",
+    title: "Front-end Developer",
+    email: "lindsay.walton@example.com",
+    role: "Member",
+  },
+  {
+    id: 2,
+    name: "Courtney Henry",
+    title: "Designer",
+    email: "courtney.henry@example.com",
+    role: "Admin",
+  },
+  {
+    id: 3,
+    name: "Tom Cook",
+    title: "Director of Product",
+    email: "tom.cook@example.com",
+    role: "Member",
+  },
+  {
+    id: 4,
+    name: "Whitney Francis",
+    title: "Copywriter",
+    email: "whitney.francis@example.com",
+    role: "Admin",
+  },
+  {
+    id: 5,
+    name: "Leonard Krasner",
+    title: "Senior Designer",
+    email: "leonard.krasner@example.com",
+    role: "Owner",
+  },
+  {
+    id: 6,
+    name: "Floyd Miles",
+    title: "Principal Designer",
+    email: "floyd.miles@example.com",
+    role: "Member",
+  },
+];
 </script>
