83f14d3e50c337b7525aa91c060a3f51978ff9a1
kapil verma <vermakp39@gmail.com>
2024-04-06 17:04:47 +0530

    code margeing

--- components/repositories/Header.vue
+++ components/repositories/Header.vue
@@ -5,13 +5,13 @@
                 <img src="/public/assets/Icons/repositories.svg" alt="repositories">
                 <p class=" font-roboto font-bold text-xl sm:text-3xl sm:leading-[48px]">Repositories</p>
             </div>
-            <div class=" flex items-center gap-2 w-full">
-                <p class=" font-roboto text-base sm:text-xl sm:leading-[30px] font-normal text-darkSlateGray">Manage
+            <div class=" flex  gap-2 w-full justify-end sm:pr-4">
+                <p class=" font-roboto text-base sm:text-xl sm:leading-[30px] font-normal text-darkSlateGray mt-[-3px]">Manage
                     Repositories</p>
                 <div class=" relative">
                     <UPopover class="table_popup">
                         <div label="Open"
-                            class="flex items-center gap-x-2 pr-1 max-h-[25px] h-full max-w-[119px] rounded-[10px]  w-full">
+                            class="flex items-center gap-x-2  max-h-[25px] h-full max-w-[119px] rounded-[10px]  w-full">
                             <IconsPopupPlus />
                         </div>
 
