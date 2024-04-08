6b4595e52f9544af587fc1da989ac591995dd0e5
kapil verma <vermakp39@gmail.com>
2024-04-08 09:34:33 +0530

    code modal working

--- components/common/Modal.vue
+++ components/common/Modal.vue
@@ -6,19 +6,19 @@
                     Create AI Remedation
                 </span>
             </UButton>
-            <UModal v-model="isOpen" :overlay="true" class="relative">
+            <UModal v-model="isOpen" :overlay="true" class="relative common_modal">
                 <Icon @click="isOpen = false" name="i-heroicons-x-mark-20-solid"
                     class=" text-black  w-6 h-6 cursor-pointer absolute end-0 top-0 mt-2 mr-3" />
-                <div class="bg-[#9E95F5] pb-[57px]">
-                    <div class=" pt-[92px] ps-[79px] pr-[70px]">
+                <div class=" bg-white p-5 md:p-0 md:pb-[57px]">
+                    <div class=" pt-14 md:pt-[92px] md:ps-[79px] md:pr-[70px]">
                         <div class="flex gap-3 items-center border-b border-black pb-5">
-                            <span class="bg-purple w-[26px] h-[26px] rounded-[50%]"></span>
+                            <span class="bg-white w-[26px] h-[26px] rounded-[50%]"></span>
                             <p class=" font-roboto text-[30px] leading-9 font-bold text-black ">Details</p>
                         </div>
-                        <p class="font-roboto text-[30px] leading-9 font-bold text-black mt-[23px] max-w-[600px]">[Asset
-                            plexicus/-OWASPWebGoatPHP] Command Injection - lastrss.php: 71</p>
+                        <p class="font-roboto text-xl md:text-[30px] leading-[120%] md:leading-9 font-bold text-black mt-[23px] max-w-[600px]">[Asset
+                            plexicus/-OWASPWebGoatPHP] Command Injection - lastrss.php: 71</p>
                         <p class="font-roboto text-xl leading-9 font-normal mt-4 border-b border-black pb-3">Ready</p>
-                        <p class="font-roboto text-base font-normal text-black leading-5 mt-[18px]">I wrapped the
+                        <p class="font-roboto text-sm sm:text-base font-normal text-black leading-[120%] sm:leading-5 mt-[18px]">I wrapped the
                             content of
                             the file
                             with base64 encoding/decoding to prevent injection through
@@ -27,13 +27,13 @@
                             into the program. Instead, any injected code will merely be treated as base64 encoded
                             string,
                             not executable code.</p>
-                        <p class="font-roboto text-base font-normal text-black leading-5 py-5">The recommendation for
+                        <p class="font-roboto text-sm sm:text-base font-normal text-black leading-[120%] sm:leading-5 py-5">The recommendation for
                             future developments is to never trust external inputs or have them in a place where they
                             could
                             somehow be executed. Instead, it would be better always to store a serialized form of the
                             data
                             and apply the necessary encoding/decoding steps on read/write operations.</p>
-                        <p class="font-roboto text-base font-normal text-black leading-5">Note that the base64_encode()
+                        <p class="font-roboto text-sm sm:text-base font-normal text-black leading-[120%] sm:leading-5">Note that the base64_encode()
                             and
                             base64_decode() functions used here are part of native PHP, so they don't require importing
                             any
@@ -45,10 +45,11 @@
 
                     </div>
                     <img src="/public/assets/images/finding-detail-modal/captura-de-pantalla.webp"
-                        alt="captura-de-pantalla" class="max-w-[1001px] w-full min-h-[312px] ps-8 mt-7">
-                    <div class=" flex justify-end">
+                        alt="captura-de-pantalla"
+                        class="lg:max-w-[1001px] w-full lg:min-h-[312px] px-4 lg:px-0 lg:ps-8 mt-7">
+                    <div class=" flex justify-end max-w-[1001px] px-4 lg:px-0">
                         <UButton
-                            class="!font-roboto font-normal !text-xl text-white bg-purple py-3 px-4  sm:px-[22px] sm:py-[5px] mt-[71px] !rounded-[0px] border border-purple hover:bg-white hover:text-purple transition-all ease-in-out duration-300 leading-9">
+                            class=" !font-roboto font-normal !text-xl text-white bg-purple py-3 px-4  sm:px-[22px] sm:py-[5px] mt-10 md:mt-[71px] !rounded-[0px] border border-purple hover:bg-white hover:text-purple transition-all ease-in-out duration-300 leading-9">
                             Send Pull Request
                         </UButton>
                     </div>
@@ -61,8 +62,8 @@
 const isOpen = ref(false)
 </script>
 <style>
-.UButton_modal .UModal {
-    max-width: 1102px !important;
-    width: 100% !important;
+
+.common_modal #headlessui-dialog-panel-nHAJMhYDoe7_4{
+max-width: 1102px !important;
 }
 </style>
