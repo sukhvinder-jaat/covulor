fb030b4eeb5401c0ee5e3d16bf1ef942d2ff4cfa
Preeti <preetiyadav5443@gmail.com>
2024-04-08 15:04:45 +0530

    changes in select

--- components/common/Select.vue
+++ components/common/Select.vue
@@ -1,24 +1,24 @@
-<template>
-  <div class="w-full">
-    <USelect
-      color="white"
-      variant="outline"
-      :options="option"
-      v-model="selectedRepository"
-    />
-    <span v-if="uploadError" class="text-red-500 text-sm leading-none">{{
-      getErrorMessage
-    }}</span>
-  </div>
-</template>
 <script setup lang="ts">
-import { ref } from "vue";
-const selectedRepository = ref("");
-const uploadError = ref(false);
-const getErrorMessage = computed(() =>
-  selectedRepository.value === "" ? "Select a repo" : ""
-);
-defineProps({
-  option: Array,
-});
+import { defineProps, defineEmits, ref } from 'vue';
+
+interface Option {
+    label: string;
+    value: string;
+}
+
+const props = defineProps<{
+    options: any;
+}>();
+
+const emit = defineEmits<{
+    (e: 'update:modelValue', value: string): void;
+}>();
+
+const country = ref(props.options ? props.options[0].value : null);
 </script>
+
+<template>
+    <div class="w-full">
+        <USelect :options="options" :modelValue="country" @update:modelValue="(value: string) => (country = value)" />
+    </div>
+</template>
