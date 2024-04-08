6e79ded8cc9e6073017025a662f22d9ec8237674
sukhvinder_jaat <122784772+sukhvinder-jaat@users.noreply.github.com>
2024-04-08 17:20:02 +0530

    table issue resolve

--- layouts/home.vue
+++ layouts/home.vue
@@ -1,38 +1,42 @@
 <template>
-    <div class="flex justify-center">
-        <SideBar :isSidebarVisible="isSidebarVisible" :sidebarHandler="toggleSidebarVisibility"
-            @closeClicked="toggleSidebarVisibility" />
-        <div class="w-full">
-            <Header @menuClicked="toggleSidebarVisibility" />
-            <Content>
-                <slot />
-            </Content>
-        </div>
-        <div @click="toggleSidebarVisibility" v-if="isSidebarVisible"
-            class="fixed inset-0 bg-black opacity-50 z-50 lg:hidden">
-        </div>
+  <div class="flex justify-center">
+    <SideBar
+      :isSidebarVisible="isSidebarVisible"
+      :sidebarHandler="toggleSidebarVisibility"
+      @closeClicked="toggleSidebarVisibility"
+    />
+    <div class="w-full overflow-hidden">
+      <Header @menuClicked="toggleSidebarVisibility" />
+      <Content>
+        <slot />
+      </Content>
     </div>
-    <Footer />
-
+    <div
+      @click="toggleSidebarVisibility"
+      v-if="isSidebarVisible"
+      class="fixed inset-0 bg-black opacity-50 z-50 lg:hidden"
+    ></div>
+  </div>
+  <Footer />
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { ref, watch } from "vue";
 
 // Define reactive variable for sidebar visibility
 const isSidebarVisible = ref(false);
 
 // Function to toggle sidebar visibility
 const toggleSidebarVisibility = () => {
-    isSidebarVisible.value = !isSidebarVisible.value;
+  isSidebarVisible.value = !isSidebarVisible.value;
 };
 
 // Watch for changes in sidebar visibility to control body overflow
 watch(isSidebarVisible, (newValue) => {
-    if (newValue) {
-        document.body.classList.add("overflow-y-hidden");
-    } else {
-        document.body.classList.remove("overflow-y-hidden");
-    }
+  if (newValue) {
+    document.body.classList.add("overflow-y-hidden");
+  } else {
+    document.body.classList.remove("overflow-y-hidden");
+  }
 });
 </script>
