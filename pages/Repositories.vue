83d0fd2632fcca0c086290f48a64d0c63053b599
Preeti <preetiyadav5443@gmail.com>
2024-04-09 16:28:00 +0530

    add table data control bar

--- pages/Repositories.vue
+++ pages/Repositories.vue
@@ -1,49 +1,20 @@
-<template class="overflow-hidden">
+<template>
   <div class="lg:pl-[54px] lg:pr-[63px] md:px-10 px-3 sm:px-5 pt-[36px] w-full">
     <RepositoriesHeader />
-    <div class="mt-16 sm:flex gap-3 sm:gap-0 justify-between items-center">
-      <div class="flex gap-3 sm:gap-[18px] items-center sm:w-6/12">
-        <CommonFilter />
-        <div class="sm:max-w-[387px] w-full">
-          <CommonSearch />
-        </div>
-      </div>
-      <div class="flex items-center gap-2 sm:gap-5 pr-4 sm:mt-0 mt-2 justify-end">
-        <UButton
-          class="font-inter font-semibold text-[13px] text-white bg-purple py-2 px-3 sm:px-[17px] sm:py-[10px] rounded-md border border-purple hover:bg-white hover:text-purple transition-all ease-in-out duration-300 leading-[15px]">
-          Download
-        </UButton>
-        <UButton
-          class="font-inter font-semibold text-[13px] !text-purple !bg-transparent uppercase !shadow-none hover:!bg-transparent flex items-center gap-2 !p-0 !border-0">
-          REFRESCH
-          <span class="hidden sm:block">
-            <img src="/assets/Icons/refresch.svg" alt="refresh" />
-          </span>
-        </UButton>
-      </div>
-    </div>
+    <CommonTableDataControlBar />
     <RepositoriesTable />
-
   </div>
 </template>
 <script setup lang="ts">
-const countries = ["United States", "Canada", "Mexico"];
 // Page Meta
 definePageMeta({
   layout: "home",
 });
-const tagsOption = ["Github", "Bitbucket", "GitLab"];
-
 
 </script>
+
 <style setup>
 #headlessui-popover-panel-ndcv2Y3vSTA_23 {
   overflow: visible !important;
 }
-
-/* .repo_pagination button {
-  background-color: transparent !important;
-  border: none;
-  color: #6e6893;
-} */
 </style>
--- pages/findings.vue
+++ pages/findings.vue
@@ -3,27 +3,7 @@
         <FindingsReports />
         <FindingsColumnChart />
         <FindingsUploadScanModal />
-        <div class="mt-16 sm:flex gap-3 sm:gap-0 justify-between items-center">
-            <div class="flex gap-3 sm:gap-[18px] items-center sm:w-6/12">
-                <CommonFilter />
-                <div class="sm:max-w-[387px] w-full">
-                    <CommonSearch />
-                </div>
-            </div>
-            <div class="flex items-center gap-2 sm:gap-5 pr-4 sm:mt-0 mt-2 justify-end">
-                <UButton
-                    class="font-inter font-semibold text-[13px] text-white bg-purple py-2 px-3 sm:px-[17px] sm:py-[10px] rounded-md border border-purple hover:bg-white hover:text-purple transition-all ease-in-out duration-300 leading-[15px]">
-                    Download
-                </UButton>
-                <UButton
-                    class="font-inter font-semibold text-[13px] !text-purple !bg-transparent uppercase !shadow-none hover:!bg-transparent flex items-center gap-2 !p-0 !border-0">
-                    REFRESCH
-                    <span class="hidden sm:block">
-                        <img src="/assets/Icons/refresch.svg" alt="refresch" />
-                    </span>
-                </UButton>
-            </div>
-        </div>
+        <CommonTableDataControlBar />
         <FindingsConfidenceTable />
     </div>
 </template>
